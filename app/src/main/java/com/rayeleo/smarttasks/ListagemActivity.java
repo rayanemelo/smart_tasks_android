ea1e99187c4fcd20e4c04c4f93a6653365d3ce1f
passosleo <leonardopassos98@gmail.com>
2025-04-14 22:11:19 -0300

    feat: list tasks

--- app/src/main/java/com/rayeleo/smarttasks/ListagemActivity.java
+++ app/src/main/java/com/rayeleo/smarttasks/ListagemActivity.java
@@ -2,12 +2,14 @@ package com.rayeleo.smarttasks;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.rayeleo.smarttasks.adapters.TarefaAdapter;
@@ -29,6 +31,9 @@ public class ListagemActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerViewTarefas);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         tarefaRepository = new TarefaRepository(this);
         tarefaRepository.open();
